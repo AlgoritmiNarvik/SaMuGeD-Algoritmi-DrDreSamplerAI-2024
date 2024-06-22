2b93e72ffb37eb862236521da7adebd32a54a919
Asle-Oren <asleoren@gmail.com>
2024-06-22 11:30:46 +0200

    Added a jupyter notebook version of the segmenter test script.

--- testing tools/test_scripts/segmenter_test.py
+++ testing tools/test_scripts/segmenter_test.py
@@ -1,9 +1,28 @@
 import sf_segmenter
 import os
 import sys
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 INPUT_PATH = "take_on_me.mid"
+CONFIG = {
+    "M_gaussian": 27,
+    "m_embedded": 3,
+    "k_nearest": 0.04,
+    "Mp_adaptive": 28,
+    "offset_thres": 0.05,
+    "bound_norm_feats": np.inf  # min_max, log, np.inf,
+                                # -np.inf, float >= 0, None
+    # For framesync features
+    # "M_gaussian"    : 100,
+    # "m_embedded"    : 3,
+    # "k_nearest"     : 0.06,
+    # "Mp_adaptive"   : 100,
+    # "offset_thres"  : 0.01
+}
+
+
+
 project_directory = os.path.dirname(os.path.realpath(__file__))
 OS_TYPE = sys.platform
 
@@ -14,7 +33,7 @@ else:
     output_dir = project_directory + "/plots"
     input_file_path = project_directory + "/" + INPUT_PATH
 
-segmenter = sf_segmenter.Segmenter()
+segmenter = sf_segmenter.Segmenter(config=CONFIG)
 
 if INPUT_PATH[-4:] == ".mid":
     segmenter.proc_midi(input_file_path)
@@ -22,6 +41,7 @@ else:
     segmenter.proc_audio(input_file_path)
 
 segmenter.plot(output_dir)
+#plt.show()
 
 print(f'Plots have been saved to: {output_dir} \n Press enter to close this window')
 input() #To prevent window from closing before being able to read text
