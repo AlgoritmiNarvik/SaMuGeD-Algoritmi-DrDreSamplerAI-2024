b695f1beca28dd40fadff20e4b39405cdf481fba
Asle-Oren <asleoren@gmail.com>
2024-09-20 13:40:25 +0200

    Updated structure of code

--- testing_tools/test_scripts/asle_scripts/sample_finder.py
+++ testing_tools/test_scripts/asle_scripts/sample_finder.py
@@ -1,9 +1,8 @@
 import tkinter as tk
-from tkinter import RAISED, SUNKEN, filedialog
+#from tkinter import RAISED, SUNKEN, filedialog
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from miditoolkit.midi.parser import MidiFile
-import numpy as np
 import pygame
 
 
@@ -11,18 +10,17 @@ pygame.init()
 pygame.mixer.init()
 
 def open_midi_file():
-    global current_midi_path
-    filepath = filedialog.askopenfilename(filetypes=[("MIDI files", "*.mid *.midi")])
+    filepath = tk.filedialog.askopenfilename(filetypes=[("MIDI files", "*.mid *.midi")])
     if not filepath:
         return
     current_midi_path = filepath
     midi_file = MidiFile(filepath)
     notes = extract_notes(midi_file)
     plot_piano_roll_original(notes)
+    print(canvas_frame.winfo_geometry())
 
 def open_midi_file_similar():
-    global current_midi_path
-    filepath = filedialog.askopenfilename(filetypes=[("MIDI files", "*.mid *.midi")])
+    filepath = tk.filedialog.askopenfilename(filetypes=[("MIDI files", "*.mid *.midi")])
     if not filepath:
         return
     current_midi_path = filepath
@@ -30,25 +28,23 @@ def open_midi_file_similar():
     notes = extract_notes(midi_file)
     plot_piano_roll_similar(notes)
 
-
 def play_midi():
-    #play_button.config(relief=SUNKEN)
+    #play_button.config(relief=tk.SUNKEN)
     if current_midi_path:
         print(pygame.mixer.music.get_busy())
         pygame.mixer.music.stop()
         pygame.mixer.music.load(current_midi_path)
         pygame.mixer.music.play()
-        print(pygame.mixer.music.get_busy())
-        a = pygame.mixer.music.get_endevent()
-        print(f'{a=}')
+        #print(pygame.mixer.music.get_busy())
+        #a = pygame.mixer.music.get_endevent()
+        #print(f'{a=}')
 
 def stop_midi():
     pygame.mixer.music.stop()
-    play_button.config(relief=RAISED)
+    #play_button.config(relief=tk.RAISED)
 
-def music_ended():
-    play_button.config(relief=RAISED)
-
+def music_ended(): # currently unused, was intended to make play button clickable again after music has stopped playing
+    play_button.config(relief=tk.RAISED)
 
 def extract_notes(midi_file):
     notes = midi_file.instruments[0].notes
@@ -84,7 +80,8 @@ def plot_piano_roll_similar(notes):
     canvas_similar.draw()
 
 
-def scrollbar():
+def build_scrollbar():
+    scrollbar_frame = tk.Frame()
     scroll_bar = tk.Scrollbar(scrollbar_frame) 
     scroll_bar.pack( side = tk.RIGHT, fill = tk.Y ) 
     mylist = tk.Listbox(scrollbar_frame, yscrollcommand = scroll_bar.set ) 
@@ -93,46 +90,50 @@ def scrollbar():
         mylist.insert(tk.END, "temp" + str(line)) 
     mylist.pack(side = tk.RIGHT, fill = tk.BOTH, expand=1) 
     scroll_bar.config(command = mylist.yview ) 
+    scrollbar_frame.pack(side=tk.LEFT, fill=tk.BOTH)
+
+def build_buttons():
+    global load_button, play_button, stop_button, temp_similar_button
+    load_button = tk.Button(window, text="Load MIDI", command=open_midi_file)
+    load_button.pack(side=tk.BOTTOM)
+
+    play_button = tk.Button(window, text="Play MIDI", command=play_midi)
+    play_button.pack(side=tk.BOTTOM)
+
+    stop_button = tk.Button(window, text="Stop MIDI", command=stop_midi)
+    stop_button.pack(side=tk.BOTTOM)
+
+    temp_similar_button = tk.Button(window, text="temp Similar button", command=open_midi_file_similar)
+    temp_similar_button.pack(side=tk.BOTTOM)
+
+def build_canvas():
+    global canvas_original, canvas_similar, canvas_frame
+    canvas_frame = tk.Frame()
+    canvas_original = FigureCanvasTkAgg(None, master=canvas_frame)
+    canvas_original.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+
+    canvas_similar = FigureCanvasTkAgg(None, master=canvas_frame)
+    canvas_similar.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    canvas_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
+
+def main():
+    global window, current_midi_path
+    current_midi_path = None
+
+    window = tk.Tk()
+    window.title("MIDI Piano Roll Viewer")
+
+    
+    build_canvas()
+    build_buttons()
+    build_scrollbar() # build scrollbar
 
 
-window = tk.Tk()
-window.title("MIDI Piano Roll Viewer")
-
-current_midi_path = None
-
-load_button = tk.Button(window, text="Load MIDI", command=open_midi_file)
-load_button.pack(side=tk.BOTTOM)
-
-play_button = tk.Button(window, text="Play MIDI", command=play_midi)
-play_button.pack(side=tk.BOTTOM)
-
-stop_button = tk.Button(window, text="Stop MIDI", command=stop_midi)
-stop_button.pack(side=tk.BOTTOM)
-
-temp_similar_button = tk.Button(window, text="temp Similar button", command=open_midi_file_similar)
-temp_similar_button.pack(side=tk.BOTTOM)
+    window.mainloop()
 
 
-canvas_frame = tk.Frame()
-scrollbar_frame = tk.Frame()
-
-
-canvas_original = FigureCanvasTkAgg(None, master=canvas_frame)
-canvas_original.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
-
-canvas_similar = FigureCanvasTkAgg(None, master=canvas_frame)
-canvas_similar.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-scrollbar() # build scrollbar
-
-canvas_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
-scrollbar_frame.pack(side=tk.LEFT, fill=tk.BOTH)
-
-
-window.mainloop()
-
-
-
+if __name__ == "__main__":
+    main()
 
 #two canvases, one for user sample, one for returned sample
 #List
